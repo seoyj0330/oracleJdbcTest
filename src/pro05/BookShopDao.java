98ea974eb43093bb9a7a52e54ffcae6c43bbf854
BIT-USER <BIT-USER@DESKTOP-9TILF22>
2018-01-19 16:48:34 +0900

    1st

--- src/pro05/BookShopDao.java
+++ src/pro05/BookShopDao.java
@@ -1,5 +1,206 @@
 package pro05;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import pro04.MemberVo;
+
 public class BookShopDao {
 
+	public void insert(BookVo vo) {
+	
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "insert into bookshop values(seq_author_id.nextval, ? , null , null , ? , '1' )";
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, vo.getTitle()); 
+			pstmt.setString(2, vo.getAuthorName());
+			
+			int count = pstmt.executeUpdate();			
+			
+			// 4.결과처리
+			System.out.println(count + "건 저장완료");
+			
+		} catch (ClassNotFoundException e) {
+			
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+	
+		} catch (SQLException e) {
+		
+			System.out.println("error:" + e);
+
+		} finally {
+
+			// 5. 자원정리
+			try {
+				if (pstmt != null) {
+	
+					pstmt.close();
+				}
+				
+				if (conn != null) {
+				
+					conn.close();
+				}
+			
+			} catch (SQLException e) {
+				
+				System.out.println("error:" + e);
+			}
+
+		}
+		
+	}
+	
+	public void rent(int num) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = " update bookshop set state_code = '0' " + 
+							" where id = ? " ;
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setInt(1, num);
+
+			pstmt.executeUpdate();			
+			
+			// 4.결과처리
+			
+			
+		} catch (ClassNotFoundException e) {
+			
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+	
+		} catch (SQLException e) {
+		
+			System.out.println("error:" + e);
+
+		} finally {
+
+			// 5. 자원정리
+			try {
+				if (pstmt != null) {
+	
+					pstmt.close();
+				}
+				
+				if (conn != null) {
+				
+					conn.close();
+				}
+			
+			} catch (SQLException e) {
+				
+				System.out.println("error:" + e);
+			}
+
+		}
+
+	
+	}
+	
+	public List<BookVo> getListAll() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<BookVo> bookList = new ArrayList<BookVo>();
+
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+		    String query = " select id "+
+					    	      " ,title "+
+					    	      " ,pubs "+
+					    	      " ,pub_date "+
+					    	      " ,author_name "+
+					    	      " ,state_code "+
+					    	" from bookshop ";
+		    
+		    pstmt = conn.prepareStatement(query);
+		    rs = pstmt.executeQuery();
+		   
+		    // 4.결과처리
+		    while(rs.next()) {
+		    	
+		    	BookVo vo = new BookVo();
+		    	
+		    	int id = rs.getInt("id");
+		    	String title = rs.getString("title");
+			    String pubs = rs.getString("pubs");
+			    String pub_date = rs.getString("pub_date");
+			    String author_name = rs.getString("author_name");
+			    String state_code = rs.getString("state_code");
+			    
+			    vo.setId(id);
+			    vo.setTitle(title);
+			    vo.setPubs(pubs);
+			    vo.setPub_date(pub_date);
+			    vo.setAuthorName(author_name);
+			    vo.setState_code(state_code);
+			   
+			    bookList.add(vo);
+			    
+				    
+		    }
+		    
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		        if (rs != null) {
+		            rs.close();
+		        }                
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+		 return bookList;
+	}
+	
 }
+	
+
--- src/pro05/BookVo.java
+++ src/pro05/BookVo.java
@@ -2,4 +2,129 @@ package pro05;
 
 public class BookVo {
 
+	private int id;
+	private String title;
+	private String pubs;
+	private String pub_date;
+	private String authorName;
+	private String state_code;
+	
+	
+	
+	public BookVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	public BookVo(String title, String pubs, String pub_date, String authorName, String state_code) {
+		super();
+		this.title = title;
+		this.pubs = pubs;
+		this.pub_date = pub_date;
+		this.authorName = authorName;
+		this.state_code = state_code;
+	}
+
+
+
+	public BookVo(int id, String title, String pubs, String pub_date, String authorName, String state_code) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.pubs = pubs;
+		this.pub_date = pub_date;
+		this.authorName = authorName;
+		this.state_code = state_code;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+	public String getPubs() {
+		return pubs;
+	}
+
+
+
+	public void setPubs(String pubs) {
+		this.pubs = pubs;
+	}
+
+
+
+	public String getPub_date() {
+		return pub_date;
+	}
+
+
+
+	public void setPub_date(String pub_date) {
+		this.pub_date = pub_date;
+	}
+
+
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+
+
+	public String getState_code() {
+		return state_code;
+	}
+
+
+
+	public void setState_code(String state_code) {
+		this.state_code = state_code;
+	}
+
+
+
+	@Override
+	public String toString() {
+		if(state_code.equals("1")) {
+			state_code = "재고있음";
+		}else if(state_code.equals("0")) {
+			state_code = "대여중";
+		}
+		return "BookVo [id=" + id + ", title=" + title + ", pubs=" + pubs + ", pub_date=" + pub_date + ", authorName="
+				+ authorName + ", state_code=" + state_code + "]";
+	}
+	
+	
+	
+	
 }
